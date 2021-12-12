6b81f4a631f511a6336dbbb5ca0a24b778cf2f8c
Puta-quePARIU <95884462+Puta-quePARIU@users.noreply.github.com>
2021-12-12 15:31:41 -0300

    Add files via upload

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -15,7 +15,8 @@ class CoolUtil
 	public static var difficultyStuff:Array<Dynamic> = [
 		['Easy', '-easy'],
 		['Normal', ''],
-		['Hard', '-hard']
+		['Hard', '-hard'],
+		['Sans', '-sans']
 	];
 
 	public static function difficultyString():String
